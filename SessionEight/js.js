eb2d3643b33431457facb3403516ecd8378c9a0e
kaaveh <kaaveh@protonmail.com>
2019-02-09 21:19:26 +0330

    Update until session eight

---
+++ SessionEight/js.js
@@ -0,0 +1,20 @@
+/*
+let hour = 19;
+
+if(hour >= 6 && hour < 12){
+    console.log('Good morning');
+} else if(hour >= 12 && hour <= 18){
+    console.log('Good afternoon');
+} else if(hour > 18 && hour < 21){
+    console.log('Good evening');
+} else {
+    console.log('Good night');
+}
+*/
+/*
+for(let i = 0; i < 5; i++){
+    console.log('Hello Worlds!');
+}
+*/
+for(let i = 0; i < 10; i++)
+    if(i % 2 !== 0) console.log(i);
